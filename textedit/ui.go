caf4ecd92aba277d46b39a6cfe4a45a7c599b9d8
Andy Williams <andy@andy.xyz>
2019-07-06 22:37:40 +0100

    Add a menu to the text editor demo

--- textedit/ui.go
+++ textedit/ui.go
@@ -14,11 +14,40 @@ import (
 type textEdit struct {
 	cursorRow, cursorCol *widget.Label
 	entry                *widget.Entry
+	window               fyne.Window
 }
 
 func (e *textEdit) updateStatus() {
 	e.cursorRow.SetText(fmt.Sprintf("%d", e.entry.CursorRow+1))
 	e.cursorCol.SetText(fmt.Sprintf("%d", e.entry.CursorColumn+1))
+}
+
+func (e *textEdit) cut() {
+	e.entry.TypedShortcut(&fyne.ShortcutCut{Clipboard: e.window.Clipboard()})
+}
+
+func (e *textEdit) copy() {
+	e.entry.TypedShortcut(&fyne.ShortcutCopy{Clipboard: e.window.Clipboard()})
+}
+
+func (e *textEdit) paste() {
+	e.entry.TypedShortcut(&fyne.ShortcutPaste{Clipboard: e.window.Clipboard()})
+}
+
+func (e *textEdit) buildToolbar() *widget.Toolbar {
+	return widget.NewToolbar(widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {
+			e.entry.SetText("")
+		}),
+		widget.NewToolbarSeparator(),
+		widget.NewToolbarAction(theme.ContentCutIcon(), func() {
+			e.cut()
+		}),
+		widget.NewToolbarAction(theme.ContentCopyIcon(), func() {
+			e.copy()
+		}),
+		widget.NewToolbarAction(theme.ContentPasteIcon(), func() {
+			e.paste()
+		}))
 }
 
 // Show loads a new text editor
@@ -27,40 +56,41 @@ func Show(app fyne.App) {
 	window.SetIcon(icon.TextEditorBitmap)
 
 	entry := widget.NewMultiLineEntry()
-	toolbar := widget.NewToolbar(widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {
-		entry.SetText("")
-	}),
-		widget.NewToolbarSeparator(),
-		widget.NewToolbarAction(theme.ContentCutIcon(), func() {
-			entry.TypedShortcut(&fyne.ShortcutCut{Clipboard: window.Clipboard()})
-		}),
-		widget.NewToolbarAction(theme.ContentCopyIcon(), func() {
-			entry.TypedShortcut(&fyne.ShortcutCopy{Clipboard: window.Clipboard()})
-		}),
-		widget.NewToolbarAction(theme.ContentPasteIcon(), func() {
-			entry.TypedShortcut(&fyne.ShortcutPaste{Clipboard: window.Clipboard()})
-		}))
-
 	cursorRow := widget.NewLabel("1")
 	cursorCol := widget.NewLabel("1")
 
+	editor := &textEdit{
+		cursorRow: cursorRow,
+		cursorCol: cursorCol,
+		entry:     entry,
+		window:    window,
+	}
+
+	toolbar := editor.buildToolbar()
 	status := widget.NewHBox(layout.NewSpacer(),
 		widget.NewLabel("Cursor Row:"), cursorRow,
 		widget.NewLabel("Col:"), cursorCol)
 	content := fyne.NewContainerWithLayout(layout.NewBorderLayout(toolbar, status, nil, nil),
 		toolbar, status, widget.NewScrollContainer(entry))
 
-	window.SetContent(content)
-	window.Resize(fyne.NewSize(480, 320))
-
-	editor := &textEdit{
-		cursorRow: cursorRow,
-		cursorCol: cursorCol,
-		entry:     entry,
-	}
 	editor.entry.OnCursorChanged = func() {
 		editor.updateStatus()
 	}
 
+	window.SetMainMenu(fyne.NewMainMenu(
+		fyne.NewMenu("File",
+			fyne.NewMenuItem("New", func() {
+				editor.entry.SetText("")
+			}),
+		),
+		fyne.NewMenu("Edit",
+			fyne.NewMenuItem("Cut", editor.cut),
+			fyne.NewMenuItem("Copy", editor.copy),
+			fyne.NewMenuItem("Paste", editor.paste),
+		),
+	))
+
+	window.SetContent(content)
+	window.Resize(fyne.NewSize(480, 320))
 	window.Show()
 }

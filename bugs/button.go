5346e78b08cc080e610525ad17092dc224f1756e
Andy Williams <andy@andy.xyz>
2019-10-30 10:30:07 +0000

    Scale text better - particularly for mobile

--- bugs/button.go
+++ bugs/button.go
@@ -27,12 +27,15 @@ func (b *bugRenderer) MinSize() fyne.Size {
 // Layout the components of the widget
 func (b *bugRenderer) Layout(size fyne.Size) {
 	inner := size.Subtract(fyne.NewSize(theme.Padding()*2, theme.Padding()*2))
-	b.label.TextSize = inner.Height - 3
-
-	b.label.Resize(inner)
-	b.label.Move(fyne.NewPos(theme.Padding(), theme.Padding()))
 	b.icon.Resize(inner)
 	b.icon.Move(fyne.NewPos(theme.Padding(), theme.Padding()))
+
+	textSize := int(float32(size.Height)*.67)
+	textMin := fyne.CurrentApp().Driver().RenderedTextSize(b.label.Text, textSize, fyne.TextStyle{Bold: true})
+
+	b.label.TextSize = textSize
+	b.label.Resize(fyne.NewSize(size.Width, textMin.Height))
+	b.label.Move(fyne.NewPos(0, (size.Height - textMin.Height)/2))
 }
 
 // ApplyTheme is called when the bugButton may need to update it's look

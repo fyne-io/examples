943eb235391525f81c1652d70e2e054fd2319fd4
Andy Williams <andy@andywilliams.me>
2020-04-02 12:28:23 +0100

    Address lint warnings
    Fixes #21

--- clock/clock.go
+++ clock/clock.go
@@ -12,14 +12,14 @@ import (
 
 type clockLayout struct {
 	hour, minute, second     *canvas.Line
-	hourdot, seconddot, face *canvas.Circle
+	hourDot, secondDot, face *canvas.Circle
 
 	canvas fyne.CanvasObject
 	stop   bool
 }
 
 func (c *clockLayout) rotate(hand fyne.CanvasObject, middle fyne.Position, facePosition float64, offset, length int) {
-	rotation := math.Pi * 2 / 60 * float64(facePosition)
+	rotation := math.Pi * 2 / 60 * facePosition
 	x2 := int(float64(length) * math.Sin(rotation))
 	y2 := int(float64(-length) * math.Cos(rotation))
 
@@ -34,7 +34,7 @@ func (c *clockLayout) rotate(hand fyne.CanvasObject, middle fyne.Position, faceP
 	hand.Resize(fyne.NewSize(x2, y2))
 }
 
-func (c *clockLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
+func (c *clockLayout) Layout(_ []fyne.CanvasObject, size fyne.Size) {
 	diameter := fyne.Min(size.Width, size.Height)
 	radius := diameter / 2
 	dotRadius := radius / 12
@@ -58,29 +58,29 @@ func (c *clockLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 	c.second.StrokeWidth = smallStroke
 	c.rotate(c.second, middle, float64(time.Now().Second()), 0, radius-3)
 
-	c.hourdot.StrokeWidth = stroke
-	c.hourdot.Resize(fyne.NewSize(dotRadius*2, dotRadius*2))
-	c.hourdot.Move(fyne.NewPos(middle.X-dotRadius, middle.Y-dotRadius))
-	c.seconddot.StrokeWidth = smallStroke
-	c.seconddot.Resize(fyne.NewSize(smallDotRadius*2, smallDotRadius*2))
-	c.seconddot.Move(fyne.NewPos(middle.X-smallDotRadius, middle.Y-smallDotRadius))
+	c.hourDot.StrokeWidth = stroke
+	c.hourDot.Resize(fyne.NewSize(dotRadius*2, dotRadius*2))
+	c.hourDot.Move(fyne.NewPos(middle.X-dotRadius, middle.Y-dotRadius))
+	c.secondDot.StrokeWidth = smallStroke
+	c.secondDot.Resize(fyne.NewSize(smallDotRadius*2, smallDotRadius*2))
+	c.secondDot.Move(fyne.NewPos(middle.X-smallDotRadius, middle.Y-smallDotRadius))
 	c.face.StrokeWidth = smallStroke
 }
 
-func (c *clockLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
+func (c *clockLayout) MinSize(_ []fyne.CanvasObject) fyne.Size {
 	return fyne.NewSize(200, 200)
 }
 
 func (c *clockLayout) render() *fyne.Container {
-	c.hourdot = &canvas.Circle{StrokeColor: theme.TextColor(), StrokeWidth: 5}
-	c.seconddot = &canvas.Circle{StrokeColor: theme.PrimaryColor(), StrokeWidth: 3}
+	c.hourDot = &canvas.Circle{StrokeColor: theme.TextColor(), StrokeWidth: 5}
+	c.secondDot = &canvas.Circle{StrokeColor: theme.PrimaryColor(), StrokeWidth: 3}
 	c.face = &canvas.Circle{StrokeColor: theme.TextColor(), StrokeWidth: 1}
 
 	c.hour = &canvas.Line{StrokeColor: theme.TextColor(), StrokeWidth: 5}
 	c.minute = &canvas.Line{StrokeColor: theme.TextColor(), StrokeWidth: 3}
 	c.second = &canvas.Line{StrokeColor: theme.PrimaryColor(), StrokeWidth: 1}
 
-	container := fyne.NewContainer(c.hourdot, c.seconddot, c.face, c.hour, c.minute, c.second)
+	container := fyne.NewContainer(c.hourDot, c.secondDot, c.face, c.hour, c.minute, c.second)
 	container.Layout = c
 
 	c.canvas = container
@@ -98,9 +98,9 @@ func (c *clockLayout) animate(canvas fyne.Canvas) {
 	}()
 }
 
-func (c *clockLayout) applyTheme(setting fyne.Settings) {
-	c.hourdot.StrokeColor = theme.TextColor()
-	c.seconddot.StrokeColor = theme.PrimaryColor()
+func (c *clockLayout) applyTheme(_ fyne.Settings) {
+	c.hourDot.StrokeColor = theme.TextColor()
+	c.secondDot.StrokeColor = theme.PrimaryColor()
 	c.face.StrokeColor = theme.TextColor()
 
 	c.hour.StrokeColor = theme.TextColor()
@@ -117,7 +117,7 @@ func Show(app fyne.App) {
 		clock.stop = true
 	})
 
-	canvas := clock.render()
+	content := clock.render()
 	go clock.animate(clockWindow.Canvas())
 
 	listener := make(chan fyne.Settings)
@@ -129,6 +129,6 @@ func Show(app fyne.App) {
 		}
 	}()
 
-	clockWindow.SetContent(canvas)
+	clockWindow.SetContent(content)
 	clockWindow.Show()
 }
